f0a46c94e4124587e20577d6cd8ef246d4f50331
Mert <mertskzc@gmail.com>
2019-04-17 14:36:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EncryptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testClass t = new testClass();
+            string testUrl = "whateveryouwant?id=25&somethingelse=40";
+            Console.WriteLine("Bu normal : " + (testUrl));
+            string sifreli = t.Encrypt(testUrl);
+            Console.WriteLine("Bu encrypt : " + sifreli);
+            Console.WriteLine("Bu decrypt : " + t.Decrypt(sifreli));
+
+            Console.ReadKey();
+        }
+    }
+}
